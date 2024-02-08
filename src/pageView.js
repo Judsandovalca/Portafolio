df85c7e120c0c662e414420759d074b412a9f78d
Juan David Sandoval <judsandovalca@unal.edu.co>
2024-02-07 22:38:27 -0500

    pageView

---
+++ src/pageView.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useEffect } from "react"
+import useLocation from "react-dom"
+import {useLocation} from "react-router-dom" 
+const PageView = () => {
+  const location = useLocation();
+  useEffect= (()=> {
+   window.dataLayer.push(
+    {
+        event: "page_view"
+    }
+   )
+  },[location])
+return null;
+}
+export  default PageView;
