37aad62ddcdc21dc0347a35db015309da0bb549b
Juan David Sandoval <judsandovalca@unal.edu.co>
2024-02-23 17:45:19 -0500

    fix

--- src/pageView.js
+++ src/pageView.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect } from "react"
-import useLocation from "react-dom"
+//import useLocation from "react-dom"
 import {useLocation} from "react-router-dom" 
 const PageView = () => {
   const location = useLocation();
