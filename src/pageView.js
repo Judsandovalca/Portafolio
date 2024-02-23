3010bc8de6d0517b91547190e7ed445ba85f2b6b
Juan David Sandoval <judsandovalca@unal.edu.co>
2024-02-23 17:56:51 -0500

    bbox fix

--- src/pageView.js
+++ src/pageView.js
@@ -1,16 +1,17 @@
-import React from "react";
-import { useEffect } from "react"
-import useLocation from "react-dom"
-import {useLocation} from "react-router-dom" 
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 const PageView = () => {
   const location = useLocation();
-  useEffect= (()=> {
-   window.dataLayer.push(
-    {
-        event: "page_view"
-    }
-   )
-  },[location])
-return null;
-}
-export  default PageView;
+
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "page_view"
+    });
+  }, [location]);
+
+  return null;
+};
+
+export default PageView;
